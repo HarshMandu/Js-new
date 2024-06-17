b2381e519be300eb2f9f39b9b690145ed0ddffdc
Harsh Mandviya <110848771+HarshMandu@users.noreply.github.com>
2024-06-17 10:36:14 +0000

    done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+// "use strict";
+ 
+// let name="HARSH"
+// let age=22
+
+let score="33abc"
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber));
+
