2e15ad46ab7b60724b086038822df2185a778b3e
Harsh Mandviya <110848771+HarshMandu@users.noreply.github.com>
2024-06-20 06:36:28 +0000

    done

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -3,10 +3,85 @@
 // let name="HARSH"
 // let age=22
 
-let score="33abc"
+// let score="33abc"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber=Number(score)
-console.log(typeof(valueInNumber));
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+
+//**********************OPERATIONS*************************
+
+// console.log("2"===2);
+
+//Primitive
+
+//  7 types:STRING, NUMBER, BOOLEAN, NULL , UNDEFINED, SYMBOL
+
+// const score =  100
+// const scoreValue=100.3
+
+// const isLoggedIn =false
+
+
+// REFERENCE
+
+//ARRAY,OBJECTS,FUNCTIONS
+
+// const heros=["shaktiman","naagraj","doga"]
+// let obj={
+//     name:"harsh",
+//     age=23,
+// }
+
+// const myFunction=function(){
+//     console.log("hello world")
+
+// }
+
+// //**********************************************
+
+// // STACK (PRIMITVE , HEAP(non-Primitive)
+
+// let myName="HARSH MANDVIYA"
+
+// let anothername= myName
+
+// console.log(anothername);
+
+// let user={
+//     email:"harsh@gmail.com"
+   
+// }
+
+// let userTwo=user
+// userTwo.email="dhruvi@gmail.com"
+
+// console.log(user.email);
+// console.log(userTwo.email);
+
+// const name="harsh"
+// const repoCount=50
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const url ="https:/harsh.com/harsh%20mandviya"
+
+// console.log(url.replace('%20','-'))
+
+// console.log(url.includes('sundar'))
+
+const score=400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toFixed(2));
+console.log(balance.toString);
+
+
+
+
+
 
