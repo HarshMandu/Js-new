970d126ef2b8fdbc0bd4cc465e8738506abc0dc9
Harsh Mandviya <110848771+HarshMandu@users.noreply.github.com>
2024-07-11 10:37:49 +0000

    done

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -326,6 +326,108 @@
 
 // NULLISH COALESCING OPERATOR (??):null undefined
 
+// let val1;
+// val1= 5 ?? 10
+// val1 = null ?? 100
+
+
+// console.log(val1);
+
+// LOOPS/ITERATION
+
+// for
+
+// for(let i=0; i<=10; i++){
+//     const element=i;
+//     if(element==5){
+//         console.log("5 is best nuumber");
+//     }
+//     console.log(element);
+// }
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer loop value:${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop value:${j} and outer loop ${i}`);
+        
+        
+//     }
+    
+// }
+
+// while and do while
+// let index=0
+// while (index<=10) {
+//     console.log(`number are ${index}`);
+//     index=index+2;
+    
+// }
+
+// myArray=['harsh','dhruvi','raj','princi']
+// let arr=0
+// while(arr < myArray.length){
+//     console.log(`Name is ${myArray[arr]}`);
+//     arr= arr + 1
+// }
+
+//  for of
+
+// const arr=[1,2,3,4,5,6,7,8]
+// for (const val of arr) {
+//     console.log(val);
+// }
+
+// const greetings="hello world"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+// MAPS
+
+// const map=new Map()
+// map.set('IN',"INDIA")
+// map.set('USA',"United States Of America")
+// map.set('CA',"Canada")
+// // console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,':-',value);
+// }
+
+// const myzobject={
+//     'game1':'NFS',
+//     'game2':'PUBG'
+// }
+
+// for (const [key,value] of myzobject) {
+//     console.log(key,':-',value);
+// }
+
+const myObject={
+    js:'javascript',
+    cpp:'C++',
+    rb:"ruby"
+}
+
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
